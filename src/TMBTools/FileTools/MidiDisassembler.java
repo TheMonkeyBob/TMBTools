c98db3e858d7b3ad54e38e6dedb2c3873079aa83
TheMonkeyBob <themonkeybob@hotmail.com>
2017-02-01 12:38:17 -0800

    Work started on midi disassembler

--- src/TMBTools/FileTools/MidiDisassembler.java
+++ src/TMBTools/FileTools/MidiDisassembler.java
@@ -1,9 +1,99 @@
 package TMBTools.FileTools;
 
+import java.io.*;
+
 /**
  * Created by TheMonkeyBob on 2017-01-29.
  */
 public class MidiDisassembler
 {
-    
+    RandomAccessFile input;
+    FileWriter output;
+    File input_file;
+    File output_file;
+
+    public MidiDisassembler(String inputFile)
+    {
+        input_file = new File(inputFile);
+        output_file = null;
+    }
+
+    public MidiDisassembler(String inputFile, String outputFile)
+    {
+        input_file = new File(inputFile);
+        output_file = new File(outputFile);
+    }
+
+    public MidiDisassembler(File inputFile)
+    {
+        input_file = inputFile;
+        output_file = null;
+    }
+
+    public MidiDisassembler(File inputFile, File outputFile)
+    {
+        input_file = inputFile;
+        output_file = outputFile;
+    }
+
+    public MidiDisassembler(String inputFile, File outputFile)
+    {
+        input_file = new File(inputFile);
+        output_file = outputFile;
+    }
+
+    public MidiDisassembler(File inputFile, String outputFile)
+    {
+        input_file = inputFile;
+        output_file = new File(outputFile);
+    }
+
+    public boolean dissasemble()
+    {
+        return false;
+    }
+
+    public boolean dissasemble(String fileName)
+    {
+        return false;
+    }
+
+    public boolean dissasemble(File file)
+    {
+        return false;
+    }
+
+    private boolean dissasembleMidi(File inputFile, File outputFile) throws IOException
+    {
+        input = new RandomAccessFile(inputFile, "r");
+        output = new FileWriter(outputFile);
+
+        while (input.getFilePointer() < input.length())
+        {
+            stateMachine(input.read());
+        }
+
+        return false;
+    }
+
+    //the state machine
+    private void stateMachine(int input)
+    {
+
+    }
+
+    private void variableLengthValue()
+    {
+
+    }
+
+    private void textBlock()
+    {
+
+    }
+
+    private void dataBlock()
+    {
+
+    }
 }
